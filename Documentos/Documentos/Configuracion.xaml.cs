20e752d1207b2c2522e42430cd8cc6bcd87b59a0
alumno <alumno@DESKTOP-TJSF3MI>
2019-11-28 11:53:02 +0100

    Conf. basic

---
+++ Documentos/Documentos/Configuracion.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Documentos
+{
+    /// <summary>
+    /// Lógica de interacción para Configuracion.xaml
+    /// </summary>
+    public partial class Configuracion : Window
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public Configuracion()
+        {
+            InitializeComponent();
+        }
+
+        private void aceptarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Height = int.Parse(Alto.Text);
+            Width = int.Parse(Ancho.Text);
+
+            this.Close();
+        }
+
+        private void cancelarButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Documentos/Documentos/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Documentos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void documentoButton_Click (object sender, RoutedEventArgs e)
+        {
+            Documento doc = new Documento();
+
+            //Establecemos la nueva ventana como un ventana hija
+            doc.Owner = this;
+
+            doc.Show();
+        }
+
+        private void configuracionButton_Click(object sender, RoutedEventArgs e)
+        {
+            Configuracion conf = new Configuracion();
+
+            //Establecemos la nueva ventana como un ventana hija
+            conf.Owner = this;
+                //Asignamos sus propiedades
+                conf.SizeToContent = SizeToContent.WidthAndHeight;
+                conf.ResizeMode = ResizeMode.NoResize;
+                conf.ShowInTaskbar = false;
+
+            conf.Show();
+        }
+    }
+}
